59c37b1996525e3831956db9f9d43ede5259c63a
impronez <orzaev7312@gmail.com>
2025-02-12 10:49:08 +0300

    Сделал 1 лабу

---
+++ lab1/TriangleTester/Program.cs
@@ -0,0 +1,84 @@
+﻿using System.Diagnostics;
+
+namespace TriangleTester
+{
+	internal class Program
+	{
+		private static readonly string TestsFilePath = "tests.txt";
+		private static readonly string ExePath = "Triangle.exe";
+		private static readonly string ResultsFilePath = "results.txt";
+
+		const string Error = "error";
+		const string Success = "success";
+
+		static void Main()
+		{
+			if (!File.Exists(TestsFilePath))
+			{
+				Console.WriteLine($"Tests file not exists: \"{TestsFilePath}\"");
+				return;
+			}
+
+			if (!File.Exists(ExePath))
+			{
+				Console.WriteLine($"Exe file not exists: \"{ExePath}\"");
+				return;
+			}
+			
+			using (StreamReader reader = new StreamReader(TestsFilePath))
+			using (StreamWriter writer = new StreamWriter(ResultsFilePath))
+			{
+				string? line;
+				while ((line = reader.ReadLine()) != null)
+				{
+					try
+					{
+						string[] parts = line.Split(' ');
+						if (parts.Length != 4)
+						{
+							writer.WriteLine(Error);
+							continue;
+						}
+
+						string[] numbers = parts.Take(3).ToArray();
+
+						string expectedProgramResult = parts.Last();
+
+						string arguments = $"{numbers[0]} {numbers[1]} {numbers[2]}";
+
+						string programResult = RunProgram(ExePath, arguments);
+
+						writer.WriteLine($"{(programResult == expectedProgramResult ? Success : Error)}");
+						Console.WriteLine($"Value: {line}, result: {programResult}, expected: {expectedProgramResult}");
+					}
+					catch (Exception ex)
+					{
+						Console.WriteLine($"Exception: {ex.Message}");
+						writer.WriteLine(Error);
+					}
+				}
+			}
+		}
+
+		static string RunProgram(string exePath, string arguments)
+		{
+			ProcessStartInfo startInfo = new ProcessStartInfo(exePath, arguments)
+			{
+				RedirectStandardOutput = true,
+				UseShellExecute = false,
+				CreateNoWindow = true
+			};
+
+			using (Process? process = Process.Start(startInfo))
+			{
+				if (process == null)
+				{
+					Console.WriteLine($"Failed to start process: \"{exePath}\"");
+					return Error;
+				}
+				process.WaitForExit();
+				return process.StandardOutput.ReadToEnd().Trim();
+			}
+		}
+	}
+}
---
+++ lab1/TriangleType/Program.cs
@@ -0,0 +1,89 @@
+﻿namespace TriangleType
+{
+	enum Result
+	{
+		Error, Equilateral, Isosceles, Scalene, NotTriangle
+	}
+
+	public class Triangle
+	{
+		private double[] _sides;
+
+		public Triangle(double side1, double side2, double side3)
+		{
+			_sides = new double[3];
+			_sides[0] = side1;
+			_sides[1] = side2;
+			_sides[2] = side3;
+		}
+
+		public string GetTriangleType()
+		{
+			if (IsInvalidSides())
+				return Result.Error.ToString();
+
+			if (IsNotTriangle())
+				return Result.NotTriangle.ToString();
+
+			if (IsEquilateralTriangle())
+				return Result.Equilateral.ToString();
+
+			if (IsIsoscelesTriangle())
+				return Result.Isosceles.ToString();
+
+			return Result.Scalene.ToString();
+		}
+
+		private bool IsInvalidSides()
+		{
+			return _sides[0] <= 0 && _sides[1] <= 0 && _sides[2] <= 0;
+		}
+
+		private bool IsNotTriangle()
+		{
+			return _sides[0] + _sides[1] <= _sides[2]
+				|| _sides[0] + _sides[2] <= _sides[1]
+				|| _sides[1] + _sides[2] <= _sides[0];
+		}
+
+		private bool IsEquilateralTriangle()
+		{
+			return _sides[0] == _sides[1] && _sides[1] == _sides[2];
+		}
+
+		private bool IsIsoscelesTriangle()
+		{
+			return _sides[0] == _sides[1] || _sides[1] == _sides[2] || _sides[0] == _sides[2];
+		}
+	}
+	internal class Program
+	{
+		static bool ParseArgs(string[] args, out Triangle? triangle)
+		{
+			triangle = null;
+			if (args.Length != 3)
+				return false;
+
+			if (double.TryParse(args[0], out double side1) && side1 > 0 &&
+				double.TryParse(args[1], out double side2) && side2 > 0 &&
+				double.TryParse(args[2], out double side3) && side3 > 0)
+			{
+				triangle = new Triangle(side1, side2, side3);
+				return true;
+			}
+
+			return false;
+		}
+
+		static void Main(string[] args)
+		{
+			if (!ParseArgs(args, out Triangle? triangle) || triangle is null)
+			{
+				Console.WriteLine(Result.Error.ToString());
+				return;
+			}
+
+			Console.WriteLine(triangle!.GetTriangleType());
+		}
+	}
+}
